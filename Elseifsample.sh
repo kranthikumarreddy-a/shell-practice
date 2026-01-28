f84578c2b062712f8ccc5f3085f94d6f746a85e4
kranthi <kranthireddy152@gmail.com>
2026-01-28 23:32:52 +0530

    is added

--- Elseifsample.sh
+++ Elseifsample.sh
@@ -3,9 +3,9 @@
 KKR=$NUM1
 ASR=$NUM2
 
-if [ $KKR > $ASR ]; then
+if [ "$KKR" > "$ASR" ]; then
 echo "Kranthi age is oider than Saidi"
-elif [ $KKR >= $ASR ]; then
+elif [ "$KKR" >= "$ASR" ]; then
 echo "Kranthi age is greater then Saidi"
 else
 echo "They both are equal"
