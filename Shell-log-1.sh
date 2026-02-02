f32f56d1b672ea1c72ac4d5e76e74ac184e6dae3
kranthi <kranthireddy152@gmail.com>
2026-02-02 19:58:10 +0530

    is added

--- Shell-log-1.sh
+++ Shell-log-1.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
  USER_ID=$(id -u)
+ SERVICE="$@"
 
-FOLDER_NAME="/var/lib/shell-practice"
-FILE_NAME="/var/lib/shell-practice/$0.log"
-
-
-VALIDATE() {
-if [ $1 -ne 0 ]; then
+if [ $USER_ID -ne 0 ]; then
 echo "Please run this as a  root user"
 else
-echo "Installing nginx"
+echo "Running the script"
 fi
-}
-VALIDATE $? "installing nginx"
-if [ $2 -ne 0 ]; then
-echo "Installing nginx is scuccess"
-else
-echo "Calling is failed"
-fi
+
+for i in "$@"
+do
+echo "Installing the : $SERVICE"
+dnf install $SERVICE -y
+echo "$SERVICE installed"
+done
