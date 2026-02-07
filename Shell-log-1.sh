649a195e887f52dd0e28a5895fe39b10934b37d5
kranthi <kranthireddy152@gmail.com>
2026-02-07 11:52:00 +0530

    is added

--- Shell-log-1.sh
+++ Shell-log-1.sh
@@ -6,14 +6,14 @@ LOGS_FILE="/var/log/shell-script/$0.log"
  mkdir -p $LOGs_FOLDER
 
 if [ $USER_ID -ne 0 ]; then
-echo "Please run this as a  root user"
+echo "Please run this as a  root user" | tee -a $LOGS_FILE
 else
-echo "Running the script"
+echo "Running the script" | tee -a $LOGS_FILE
 fi
 
 for SERVICE in "$@" 
 do
-echo "Installing the : $SERVICE"
+echo "Installing the : $SERVICE" | tee -a $LOGS_FILE
 dnf install $SERVICE -y   &>> $LOGS_FILE
-echo "$SERVICE installed"
+echo "$SERVICE installed" | tee -a $LOGS_FILE
 done
